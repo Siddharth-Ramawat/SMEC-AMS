e951962eef02049dbf41661d3513a420c39520e0
blackjackal982 <ssirisha639@gmail.com>
2020-03-09 19:23:42 +0530

    Added feedback app

---
+++ feedback/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Feedback(models.Model):
+
+    user_name = models.CharField(max_length=120)
+    category = models.CharField(max_length=120)
+    date_added = models.DateField(auto_now_add=True)
+    text = models.CharField(max_length=400)
+    email= models.EmailField()
+
+    def __str__(self):
+        return self.user_name
