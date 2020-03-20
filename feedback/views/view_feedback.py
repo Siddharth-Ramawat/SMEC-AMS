def6f9f04b4c6671ff7d5a9cb54e139b0fe669db
blackjackal982 <ssirisha639@gmail.com>
2020-03-20 20:22:17 +0530

    Added authentication to feedback

--- feedback/views/view_feedback.py
+++ feedback/views/view_feedback.py
@@ -4,18 +4,16 @@ from django.views import View
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from ..models import Feedback
-from django.contrib.auth.decorators import login_required
 
-
-# @login_required
-class ViewAllFeedback(LoginRequiredMixin,View):
+class ViewAllFeedback(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            if kwargs:
+                feedback_entries = Feedback.objects.filter(category=kwargs['cat'])
+                return render(request, template_name="view_feedback.html", context={'feedback': feedback_entries, 'title': 'View Feedback'})
 
-        if kwargs:
-            feedback_entries = Feedback.objects.filter(category=kwargs['cat'])
-            return render(request, template_name="view_feedback.html", context={'feedback': feedback_entries, 'title':'View Feedback'})
+            feedback_entries = Feedback.objects.all()
+            return render(request, template_name="view_feedback.html", context={'feedback': feedback_entries, 'title': 'View Feedback'})
 
-        feedback_entries = Feedback.objects.all()
-        return render(request, template_name="view_feedback.html", context={'feedback': feedback_entries, 'title':'View Feedback'})
-
+        return render(request, template_name="dash/stalker.html", context={'title': 'Alert'})
