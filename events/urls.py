07cb84a6da06f6c35cad978dff9f851e6144291b
Ashish Kamatala <kamatalaashish@gmail.com>
2020-05-07 21:28:38 +0530

    Revert "added events module and added events array in user_profile table"

--- events/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns =[
-    path(r'events/', events_creation.as_view(), name="events"),
-    path(r'view_events/', view_events.as_view(), name ="view_events"),
-]
