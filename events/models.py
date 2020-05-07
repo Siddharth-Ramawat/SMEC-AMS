a5474244bb6f4cbe98b74fe9d04382a0a24e35a0
ashish5698 <kamatalaashish@gmail.com>
2020-05-07 20:03:05 +0530

    'added_events_module_and_added_events_array_in_user_profile_table'

--- events/models.py
+++ events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from users.models import Profile
+import datetime
 
-# Create your models here.
+class Events(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date_added = models.DateField(auto_now=True)
+    event_date = models.DateField(default=datetime.date.today)
+    event_subject = models.CharField(max_length=40)
+    organizer_name = models.CharField(max_length=50)
+    text = models.CharField(max_length=400)
+    email = models.EmailField()
+    venue = models.CharField(max_length=200,default=0)
+
+    def __str__(self):
+        return self.event_subject
+
+
+class Poll(models.Model):
+    event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
+    yes_count = models.IntegerField(default=0)
+    no_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.event_id)
