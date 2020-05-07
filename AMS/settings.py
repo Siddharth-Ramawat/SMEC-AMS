fcc023e52394ebf03a116873f0ed977c2c53723d
ashish5698 <kamatalaashish@gmail.com>
2020-05-07 21:30:07 +0530

    'removed_imagekit'

--- AMS/settings.py
+++ AMS/settings.py
@@ -51,8 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mathfilters',
-    "imagekit"
+    'mathfilters'
 ]
 
 MIDDLEWARE = [
