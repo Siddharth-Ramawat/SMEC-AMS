cd596d3bce3a2c1b849287bd91f5f4f0b33416d0
blackjackal982 <ssirisha639@gmail.com>
2020-05-16 19:46:05 +0530

    modified textbox layout

--- feedback/views/insert_feedback.py
+++ feedback/views/insert_feedback.py
@@ -10,14 +10,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class FeedbackView(LoginRequiredMixin,View):
     def get(self,request, *args, **kwargs):
         initial = {'username':request.user.username}
-        form = FeedbackForm(instance=request.user, initial=initial, size = 1, maxChars=400)
+        form = FeedbackForm(instance=request.user, initial=initial, size = 8, maxChars=400)
         return render(request,
                       template_name='insert_feedback.html',
                       context={'form': form, 'title': 'Feedback Form'})
 
     def post(self, request, *args, **kwargs):
         initial = {'username':request.user.username}
-        form = FeedbackForm(request.POST, initial=initial,size=1,maxChars=400)
+        form = FeedbackForm(request.POST, initial=initial,size=8,maxChars=400)
         if form.is_valid():
             form = form.save(commit = False)
             form.user = Profile.objects.get(id=request.user.id)
