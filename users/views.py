0fa432e7209722449c4ef5e939842c3354e6a8b7
Siddharth Ramawat <30663964+Siddharth-Ramawat@users.noreply.github.com>
2020-05-16 20:02:38 +0530

    Added passout year
    added passout year option and removed right margin overflow

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
-from .forms import UserRegisterForm,ProfileRegisterForm, UserUpdateForm,ProfileUpdateForm
+from .forms import UserRegisterForm,ProfileRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
