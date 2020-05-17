f749739d1169c674f703af88cdc6966846db378c
ashish5698 <kamatalaashish@gmail.com>
2020-05-17 11:46:38 +0530

    ModifiedEvents

--- events/urls.py
+++ events/urls.py
@@ -6,4 +6,5 @@ urlpatterns =[
     path(r'view_events/', view_events.as_view(), name ="view_events"),
     path(r'event_update/<int:id>',events_creation.as_view(),name='event-update'),
     path(r'delete/',EventsDeleteView.as_view(),name='event-delete'),
+    path(r'delete_event/', DeleteSpecificEvent.as_view(),name='delete-event')
 ]
