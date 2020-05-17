f3da59f1ecb39603ddcda570007755625adc9350
Siddharth Ramawat <30663964+Siddharth-Ramawat@users.noreply.github.com>
2020-05-17 14:41:09 +0530

    added widget to event_date

--- events/forms.py
+++ events/forms.py
@@ -1,32 +1,38 @@
 from django import forms
-from .models import Events,Poll
+from .models import Events, Poll
 import datetime
 
-#date checker
+
+# date checker
 def present_or_future_date(value):
     if value < datetime.date.today():
         raise forms.ValidationError("The date cannot be in the past!")
     return value
 
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class EventsCreation(forms.ModelForm):
     text = forms.CharField(max_length=400, widget=forms.Textarea())
-    event_date = forms.DateField(help_text="MM/DD/YYYY",validators=[present_or_future_date])
+    event_date = forms.DateField(help_text="MM/DD/YYYY", validators=[present_or_future_date], widget=DateInput)
     venue = forms.CharField(empty_value='')
 
     class Meta:
-         model = Events
-         exclude = ['username','user']
+        model = Events
+        widgets = {'event_date': DateInput()}
+        exclude = ['username', 'user']
 
-    def __init__(self,*args,**kwargs):
+    def __init__(self, *args, **kwargs):
         super(EventsCreation, self).__init__(*args, **kwargs)
         instance = getattr(self, 'instance', None)
         if instance and instance.id:
             self.fields['email'].widget.attrs['readonly'] = True
+
 
 class Polls(forms.ModelForm):
     class Meta:
         model = Poll
         include = ['event_id']
         exclude = ['username']
-
-
